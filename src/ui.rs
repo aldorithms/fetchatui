bcb0d526c5ab6e491834b834200ca042c37f7a1c
Aldo A. Avalos <99375833+aldorithms@users.noreply.github.com>
2023-12-07 22:48:56 -0800

    added shell

--- src/ui.rs
+++ src/ui.rs
@@ -4,7 +4,7 @@ use ratatui::{
     widgets::{Block, BorderType, Borders, Paragraph},
     Frame,
 };
-use sysinfo::{SystemExt, DiskExt, CpuExt};
+use sysinfo::{SystemExt, CpuExt};
 use num_format::{Locale, ToFormattedString};
 
 
@@ -22,30 +22,28 @@ pub fn render(app: &mut App, frame: &mut Frame) {
     // - https://docs.rs/ratatui/latest/ratatui/widgets/index.html
     // - https://github.com/ratatui-org/ratatui/tree/master/examples
     let chunks = Layout::default()
-    .direction(Direction::Horizontal)
-    .margin(1)
-    .constraints(
-        [
-            Constraint::Percentage(50),
-            Constraint::Percentage(50),
-        ].as_ref()
-    )
-    .split(frame.size());
+        .direction(Direction::Horizontal)
+        .margin(1)
+        .constraints(
+            [
+                Constraint::Percentage(50),
+                Constraint::Percentage(50),
+            ].as_ref()
+        )
+        .split(frame.size());
 
     let icon_content = std::fs::read_to_string("icon.txt")
         .unwrap_or("".to_string());
 
-    let left_block = Paragraph::new(icon_content)
-        .block(Block::default().borders(Borders::ALL))
-        .alignment(Alignment::Left);
-
-    let right_block = Paragraph::new(format!(
-        "{}@{}\n\
+    let system_information = format!(
+        "
+        {}@{}\n\
         -------------------------------------------------\n\
         OS: {} \n\
         Host: Unimplemented \n\
         Kernel: {} \n\
         Uptime: {}d, {}h, {}m, {}s \n\
+        Shell: {} \n\
         DE: {} \n\
         CPU: {} - {}% \n\
         RAM:  {} / {} bytes - {}% \n\
@@ -59,6 +57,7 @@ pub fn render(app: &mut App, frame: &mut Frame) {
         (system.uptime().div_euclid(3600)).rem_euclid(24), // 3600 seconds in an hour
         (system.uptime().div_euclid(60)).rem_euclid(60), // 60 seconds in a minute
         system.uptime().rem_euclid(60), // 1 second per minute
+        query_shell::get_shell_name().unwrap(),
         whoami::desktop_env(),
         system.global_cpu_info().brand(), system.global_cpu_info().cpu_usage(),
         system.used_memory().to_formatted_string(&Locale::en),
@@ -68,10 +67,16 @@ pub fn render(app: &mut App, frame: &mut Frame) {
         system.total_swap().to_formatted_string(&Locale::en),
         (system.used_swap() as f64 / system.total_swap() as f64) * 100.0,
         //disks_info.join("")
-    ))
-    .block(Block::default().title("Fetchatui").title_alignment(Alignment::Center).borders(Borders::ALL).border_type(BorderType::Rounded))
-    .style(Style::default())
-    .alignment(Alignment::Left);
+    );
+
+    let left_block = Paragraph::new(icon_content)
+        .block(Block::default().borders(Borders::ALL))
+        .alignment(Alignment::Left);
+
+    let right_block = Paragraph::new(system_information)
+        .block(Block::default().title("Fetchatui").title_alignment(Alignment::Center).borders(Borders::ALL).border_type(BorderType::Rounded))
+        .style(Style::default())
+        .alignment(Alignment::Left);
 
     frame.render_widget(left_block, chunks[0]);
     frame.render_widget(right_block, chunks[1]);
